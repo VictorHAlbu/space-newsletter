f1ba38f6f921cc32fdb75e0dcb563d2f188f9fc0
Victor Hugo Albuquerque <vh12albuquerque@gmail.com>
2022-09-30 11:38:35 -0300

    feature cron rake

--- SpaceFlightNews-API/config/routes.rb
+++ SpaceFlightNews-API/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :articles do
-    resources :launches
-    resources :events
+  
+  scope '/api' do
+    resources :articles do
+      resources :launches
+      resources :events
+    end
+    get '', to: 'home#index'
   end
 
-  get '', to: 'home#index'
 end
